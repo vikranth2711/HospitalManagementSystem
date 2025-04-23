f3f88dea954bf6e3e4a875a247b2c6f20e078415
Ashish Shiv <110735303+AshishShiv101@users.noreply.github.com>
2025-04-23 16:10:17 +0530

    Update AppointmentSchedulingView.swift

--- FrontEnd/Hospitality 2/Hospitality/Views/Patient/AppointmentSchedulingView.swift
+++ FrontEnd/Hospitality 2/Hospitality/Views/Patient/AppointmentSchedulingView.swift
@@ -1,22 +1,7 @@
 import SwiftUI
 import PDFKit
 
-// Color extension for hex colors
-extension Color {
-    init(hex: String) {
-        let hex = hex.trimmingCharacters(in: .whitespacesAndNewlines).uppercased()
-        var rgb: UInt64 = 0
-        Scanner(string: hex).scanHexInt64(&rgb)
-        
-        self.init(
-            .sRGB,
-            red: Double((rgb >> 16) & 0xFF) / 255.0,
-            green: Double((rgb >> 8) & 0xFF) / 255.0,
-            blue: Double(rgb & 0xFF) / 255.0,
-            opacity: 1.0
-        )
-    }
-}
+
 
 struct AppointmentSchedulingView: View {
     @Environment(\.colorScheme) var colorScheme
